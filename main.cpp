3cbc0783507471d7490ae4dea2954e67d8ee43ca
Gigi <tyabladzegigi899@gmail.com>
2026-04-02 22:49:09 +0400

    this program prints the first digit of a two digit integer that u entered

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int a, b;
-    cout << "\n";
-    cout << "please insert two integers. \n";
-    cin >> a >> b;
-    cout << "\n";
-    cout << "the sum of the two integers are" << " " << a+b;
+    cout << "please insert an integer.";
+    cin >> a;
+    b = a/10;
+    cout << "the first digit of your integer is:" << b;
+    return 0;
 }
