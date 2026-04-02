5fa0bdbfe9d7d5bcc9c28ad73e589e9b87be7a53
tinatin tyabladze <tikotyablaze@gmail.com>
2026-04-02 20:53:35 +0400

    i made it prettier

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main()
 {
     int a, b;
-    cout << "please insert an integer";
+    cout << "\n";
+    cout << "please insert an integer\n";
     cin >> a;
-    cout << "plese insert a second integer";
+    cout << "plese insert a second integer\n";
     cin >> b;
-    cout << " the sum of your inserted numbers are" << " " << a+b;
+    cout << "the sum of your inserted numbers are" << " " << a+b;
 }
