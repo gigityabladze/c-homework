152acc3e130ff28dccdf5b9aab6b4fa91ee4f89a
tinatin tyabladze <tikotyablaze@gmail.com>
2026-04-02 20:43:03 +0400

    progam asks for two integers and tells us the sum of them

--- main.cpp
+++ main.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main()
 {
-
-}   
+    int a, b;
+    cout << "please insert an integer";
+    cin >> a;
+    cout << "plese insert a second integer";
+    cin >> b;
+    cout << " the sum of your inserted numbers are" << " " << a+b;
+}
