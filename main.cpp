83911893f2d5751947d053118077473a5072294c
Gigi <tyabladzegigi899@gmail.com>
2026-04-03 22:42:06 +0400

    i made it so the use has to enter a two digit integer and the program swaps the digitd places.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-    cout << "\nPlease enter a 4 digit integer\n";
+    cout << " Please enter a two digit integer.\n";
 
     int a;
     cin >> a;
     
-    int ans = (a/10)/10%10+(a/10)%10;
-    cout << "\nThe sum of the middle numbers in your integer is: " << ans;
+    int ans = (a%10)*10+a/10;
+    cout << "Youre integers digits swapped is: " << ans;
 }
