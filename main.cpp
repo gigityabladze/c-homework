629540c666d5cb50dd2e458f8a1ec810617573a5
Gigi <tyabladzegigi899@gmail.com>
2026-04-04 21:33:08 +0400

    i made it so the user has to enter a two digit integer and the program prints the maximum digit.

--- main.cpp
+++ main.cpp
@@ -3,18 +3,13 @@ using namespace std;
 int main()
 {
     int a, b, c, mx;
-    cout << "\nPlease enter three integers.\n";
-    cin >> a >> b >> c;
-    if (a > b && a > c)
-        mx = a;
-    
-    if (b > a && b > c)
-        mx = b;
-    
-    if (c > a && c > b)
-        mx = c;
-
-    cout << "The biggest integer in you're three integer is: " << mx;
+    cout << " enter a two digit integer\n";
+    cin >> a;
+    b = a/10;
+    c = a%10;
+    if (b > c) mx = b;
+    if (c > b) mx = c;
+    cout << "the maximum digit is " << mx;
 
     return 0;
 }
