8d70d9ad24475b7cc6771ac31372ec74aee44a88
Gigi <tyabladzegigi899@gmail.com>
2026-04-18 23:32:23 +0400

    the User has to enter two integers and the program prints the littler one first and the bigger one second

--- main.cpp
+++ main.cpp
@@ -2,28 +2,15 @@
 using namespace std;
 int main()
 {
-    int a, candys;
-    cout << "enter the number of candys";
-    cin >> candys;
-    int player = 1;
-    question:
-        cout << "player " << player;
-        cin >> a;
-        if (candys >= a)
-        {
-            candys = candys-a;
-            if (candys == 0)
-            {
-                cout << "the winner is " << player;
-                return 0;
-            }
-            if (player == 1)
-                player = 2;
-            else
-                player = 1;
-            goto question;
-        }
-        else
-            cout << "the winner is " << player;
+    int a, b, t;
+    cin >> a >> b;
+    if (a > b) 
+    {
+        t = a;
+        a = b;
+        b = t;
+    }
+    cout << a << " " << b;
+
     return 0;
 }
